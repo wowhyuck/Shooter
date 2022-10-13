25c8a96cf3283d641e4490200fe6d8c8e64aec1c
Joohyuck <joohyuck.seo@gmail.com>
2022-10-14 07:27:32 +0900

    Anim Instance 만들기

---
+++ Source/Shooter/ShooterAnimInstance.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAnimInstance.h"
+#include "ShooterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UShooterAnimInstance::UpdateAnimationProperties(float deltaTime)
+{
+	if (ShooterCharacter == nullptr)
+	{
+		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+	}
+
+	if (ShooterCharacter)
+	{
+		// 캐릭터 이동 속도
+		FVector velocity{ ShooterCharacter->GetVelocity() };
+		velocity.Z = 0;
+		speed = velocity.Size();
+
+		// 캐릭터가 공중에 떠 있는지?
+		bIsAir = ShooterCharacter->GetCharacterMovement()->IsFalling();
+
+		// 캐릭터가 움직이는지?
+		if (ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f)
+		{
+			bIsAccelerating = true;
+		}
+		else
+		{
+			bIsAccelerating = false;
+		}
+	}
+}
+
+void UShooterAnimInstance::NativeInitializeAnimation()
+{
+	ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+
+
+
+}
---
+++ Source/Shooter/ShooterAnimInstance.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ShooterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API UShooterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float deltaTime);
+
+	virtual void NativeInitializeAnimation() override;
+	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	class AShooterCharacter* ShooterCharacter;
+
+	// 캐릭터 이동 속도
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float speed;
+
+	// 캐릭터가 공중에 떠 있는지
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsAir;
+
+	// 캐릭터가 움직이는지
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
+
+
+};
