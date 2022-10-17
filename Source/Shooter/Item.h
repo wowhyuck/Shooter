8197b228052da9cc233dca69f0f4b6ebb19f496c
Joohyuck <joohyuck.seo@gmail.com>
2022-10-18 05:31:58 +0900

    Itme class 만들기
    : class 변수로 아이템 mesh와 collision box를 만들고 collion box를 mesh에 붙인다.

---
+++ Source/Shooter/Item.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class SHOOTER_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	// 아이템의 skeletal mesh
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* ItemMesh;
+
+	// Line trace (조준점 경로)와 충돌된 box (해당 HUD widget들을 보여주기 위해)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* CollisionBox;
+
+
+public:
+
+};
