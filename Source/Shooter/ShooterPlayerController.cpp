2035b8908de47431c342ccde2426a7de2f15aec8
Joohyuck <joohyuck.seo@gmail.com>
2022-11-11 23:59:43 +0900

    탄약 HUD 스크린 화면에 띄우기

---
+++ Source/Shooter/ShooterPlayerController.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterPlayerController.h"
+#include "Blueprint/UserWidget.h"
+
+AShooterPlayerController::AShooterPlayerController()
+{
+
+}
+
+void AShooterPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// HUD 오버레이 클래스 체크하기
+	if (HUDOverlayClass)
+	{
+		HUDOverlay = CreateWidget<UUserWidget>(this, HUDOverlayClass);
+		if (HUDOverlay)
+		{
+			HUDOverlay->AddToViewport();
+			HUDOverlay->SetVisibility(ESlateVisibility::Visible);
+		}
+	}
+}
---
+++ Source/Shooter/ShooterPlayerController.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ShooterPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API AShooterPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AShooterPlayerController();
+	
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+	// 모든 HUD 오버레이 블루프린트 클래스 참조
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Widgets, meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<class UUserWidget> HUDOverlayClass;
+
+	// HUD 오버레이 Widget의 변수
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Widgets, meta = (AllowPrivateAccess = "true"))
+	UUserWidget* HUDOverlay;
+
+};
