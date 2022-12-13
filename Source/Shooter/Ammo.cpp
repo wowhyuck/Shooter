6465589a800f0d2b5cb9380a2df39fbad224122f
Joohyuck <joohyuck.seo@gmail.com>
2022-12-14 03:09:40 +0900

    Ammo Class 만들기

---
+++ Source/Shooter/Ammo.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ammo.h"
+#include "Components/BoxComponent.h"
+#include "Components/WidgetComponent.h"
+#include "Components/SphereComponent.h"
+
+
+AAmmo::AAmmo()
+{
+	// AmmoMesh component 만들기 및 root component로 설정하기
+	AmmoMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("AmmoMesh"));
+	SetRootComponent(AmmoMesh);
+
+	GetCollisionBox()->SetupAttachment(GetRootComponent());
+	GetPickupWidget()->SetupAttachment(GetRootComponent());
+	GetAreaSphere()->SetupAttachment(GetRootComponent());
+}
+
+void AAmmo::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AAmmo::BeginPlay()
+{
+	Super::BeginPlay();
+}
