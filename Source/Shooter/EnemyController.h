f11161e126d1c74d44462b2d00148893bd66959e
Joohyuck <joohyuck.seo@gmail.com>
2023-02-26 06:59:07 +0900

    EnemyController(AIController) 만들고 세팅하기

---
+++ Source/Shooter/EnemyController.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "EnemyController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API AEnemyController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	AEnemyController();
+	virtual void OnPossess(APawn* InPawn) override;
+	
+private:
+	// 몬스터의 Blackboard Component
+	UPROPERTY(BlueprintReadWrite, Category = "AI Behavior", meta = (AllowPrivateAccess = "true"))
+	class UBlackboardComponent* BlackboardComponent;
+
+	// 몬스터의 Behavior Tree Component
+	UPROPERTY(BlueprintReadWrite, Category = "AI Behavior", meta = (AllowPrivateAccess = "true"))
+	class UBehaviorTreeComponent* BehaviorTreeComponent;
+	
+public:
+	FORCEINLINE UBlackboardComponent* GetBlackboardComponent() const { return BlackboardComponent; }
+	
+
+};
